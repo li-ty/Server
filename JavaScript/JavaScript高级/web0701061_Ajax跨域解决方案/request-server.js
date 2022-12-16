e228aad16e1f9ed3f75f6d7e69ac99a066524fe0
Lee <1303605700@qq.com>
2022-12-17 01:09:47 +0800

    整理

--- JavaScript/JavaScript高级/web0701061_Ajax跨域解决方案/request-server.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var app = express();
-app.get('/',function(req,res){    
-    res.sendFile(__dirname+'/index.html');
-});
-var server = app.listen(4000,function(){});
